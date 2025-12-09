128f92674f4b5b0a5b0f41056a7d66f9f4f62aac
alen899-my <alenjames899@example.com>
2025-12-09 19:00:35 +0530

    button respsonsive

--- src/components/common/PageBreadCrumb.tsx
+++ src/components/common/PageBreadCrumb.tsx
@@ -608,14 +608,15 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle, onLeadAdded }) =
           {pageTitle}
         </h2>
         <button
-          onClick={() => setIsModalOpen(true)}
-          className="
-            inline-flex items-center justify-center gap-2 
-             sm:w-auto px-4 py-2 rounded-lg text-sm font-medium shadow-sm transition-colors
-            bg-blue-600 hover:bg-blue-700 text-white
-            dark:bg-blue-600 dark:hover:bg-blue-500
-          "
-        >
+  onClick={() => setIsModalOpen(true)}
+  className="
+    inline-flex items-center justify-center gap-2 
+    w-fit sm:w-auto px-4 py-2 rounded-lg text-sm font-medium shadow-sm transition-colors
+    bg-blue-600 hover:bg-blue-700 text-white
+    dark:bg-blue-600 dark:hover:bg-blue-500
+  "
+>
+
           <Plus className="w-4 h-4" />
           Add New Lead
         </button>
